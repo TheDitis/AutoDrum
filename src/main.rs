a38ba6bb29e8568896343d784cbd9e5f1c2a267c
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2023-05-26 20:27:49 -0600

    feat: simple ble device list

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,8 @@ use rppal::system::DeviceInfo;
 
 use btleplug::api::{Central, CharPropFlags, Manager as _, Peripheral, ScanFilter};
 use btleplug::platform::Manager;
-use futures::stream::StreamExt;
 use tokio::time;
-use uuid::Uuid;
+use uuid::{Uuid, uuid};
 
 const BASE_HIT_DURATION_SMALL: f32 = 0.0002;
 const BASE_HIT_DURATION_BIG: f32 = 0.005;
@@ -17,11 +16,49 @@ const BASE_HIT_DURATION_BIG: f32 = 0.005;
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
 const GPIO_LED: u8 = 4;
 
-fn setup_midi_bluetooth() -> Result<(), Box<dyn, Error>> {
+const BLE_MIDI_SERVICE_ID: Uuid = uuid!("03B80E5A-EDE8-4B33-A751-6CE34EC4C700");
+const BLE_MIDI_CHARACTERISTIC_ID: Uuid = uuid!("7772E5DB-3868-4112-A1A9-F2669D106BF3");
 
+async fn setup_midi_bluetooth() -> Result<(), Box<dyn Error>> {
+    let manager = Manager::new().await?;
+    let adapter_list = manager.adapters().await?;
+    if adapter_list.is_empty() {
+        eprintln!("No Bluetooth adapters found");
+    }
+
+    for adapter in adapter_list.iter() {
+        println!("Starting scan...");
+        adapter
+            .start_scan(ScanFilter::default())
+            .await
+            .expect("Can't scan BLE adapter for connected devices...");
+        time::sleep(Duration::from_secs(2)).await;
+        let peripherals = adapter.peripherals().await?;
+        if peripherals.is_empty() {
+            eprintln!("->>> BLE peripheral devices were not found, sorry. Exiting...");
+        } else {
+            // All peripheral devices in range.
+            for peripheral in peripherals.iter() {
+                let properties = peripheral.properties().await?;
+                let is_connected = peripheral.is_connected().await?;
+                let local_name = properties
+                    .unwrap()
+                    .local_name
+                    .unwrap_or(String::from("(peripheral name unknown)"));
+                println!(
+                    "Peripheral {:?} is connected: {:?}",
+                    &local_name, is_connected
+                );
+            }
+        }
+    }
+    Ok(())
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    setup_midi_bluetooth().await?;
+
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
 
     let mut pin = Gpio::new()?.get(GPIO_LED)?.into_output();
