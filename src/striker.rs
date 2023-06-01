ab43bb4dcb0b3eaa23c19005ddea2c10859df7ed
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2023-05-31 18:23:59 -0600

    feat: better striker duration (big)

--- src/striker.rs
+++ src/striker.rs
@@ -6,21 +6,19 @@ pub enum Striker {
 impl Striker {
     pub fn get_duration(&self, velocity: u8) -> u64 {
         println!("Getting duration for velocity {}", velocity);
-        // self.min_hit_duration() + (velocity as u64 / 127) * self.max_hit_duration_variation()
-        // TODO: Is 40 just for this one keyboard?
-        self.min_hit_duration() + (velocity as u64 / 40) * self.max_hit_duration_variation()
+        self.min_hit_duration() + ((velocity as u64 * self.max_hit_duration_variation()) / 127)
     }
 
     fn min_hit_duration(&self) -> u64 {
         match self {
             Striker::SolenoidBig => 12,
-            Striker::SolenoidSmall => 6,
+            Striker::SolenoidSmall => 8,
         }
     }
 
     fn max_hit_duration_variation(&self) -> u64 {
         match self {
-            Striker::SolenoidBig => 20,
+            Striker::SolenoidBig => 10,
             Striker::SolenoidSmall => 10,
         }
     }
