7386dc387291b7bd82fe7c57dc8a0d5c0044026c
Ryan McKay <44735056+TheDitis@users.noreply.github.com>
2023-12-13 21:04:45 -0700

    chore: clean up dead/debug code from midi_ble

--- src/midi_ble.rs
+++ src/midi_ble.rs
@@ -1,40 +1,31 @@
-use std::error::Error;
-use std::future::Future;
-use std::io::Read;
-use std::pin::Pin;
+use std::sync::Arc;
+use std::time::Duration;
 use bluer::{
     adv::Advertisement,
     gatt::{
         local::{
-            characteristic_control, Application, Characteristic, CharacteristicControlEvent,
-            CharacteristicNotify, CharacteristicNotifyMethod, CharacteristicWrite, CharacteristicWriteMethod,
-            Service, CharacteristicRead
+            Application, Characteristic,
+            CharacteristicNotify, CharacteristicNotifyMethod, CharacteristicRead, CharacteristicWrite,
+            CharacteristicWriteMethod, Service
         },
-        CharacteristicReader, CharacteristicWriter,
     },
 };
-use std::time::Duration;
-use std::sync::Arc;
 use bluer::adv::AdvertisementHandle;
 use bluer::agent::{Agent, AgentHandle};
 use bluer::gatt::local::ApplicationHandle;
-use tokio::{
-    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    time::sleep,
-    sync::Mutex,
-};
+use futures::StreamExt;
+use tokio::sync::Mutex;
 use uuid::{Uuid, uuid};
-use futures::{future, pin_mut, StreamExt};
 
+// Specified by MIDI BLE spec, these are the UUIDs for the MIDI service and characteristic and should never change
 const BLE_MIDI_SERVICE_ID: Uuid = uuid!("03B80E5A-EDE8-4B33-A751-6CE34EC4C700");
 const BLE_MIDI_CHARACTERISTIC_ID: Uuid = uuid!("7772E5DB-3868-4112-A1A9-F2669D106BF3");
 
-const GPIO_LED: u8 = 4;
-
 type MIDIEvent = (u8, u8, u8);
 
+/// Handles the sending and receiving of MIDI data over BLE, forwarding relevant MIDI events to AutoDrum
 pub struct MidiBle {
-    midi_session: bluer::Session,
+    ble_session: bluer::Session,
     app_handle: Option<ApplicationHandle>,
     advertisement_handle: Option<AdvertisementHandle>,
     agent_handle: Option<AgentHandle>,
@@ -45,7 +36,7 @@ impl MidiBle {
     pub async fn new() -> MidiBle {
         let (tx, _rx) = tokio::sync::broadcast::channel::<MIDIEvent>(120);
         MidiBle {
-            midi_session: bluer::Session::new().await.unwrap(),
+            ble_session: bluer::Session::new().await.unwrap(),
             app_handle: None,
             advertisement_handle: None,
             agent_handle: None,
@@ -53,19 +44,16 @@ impl MidiBle {
         }
     }
 
+    /// Initialize the BLE MIDI service
+    ///
+    /// 1. Register the agent (as default and with no PIN code)
+    /// 2. Make sure the adapter is powered on and ready to go, but not discoverable or pairable outside the midi service
+    /// 3. Advertise the MIDI service with a low connection interval (specified <15ms by MIDI BLE spec)
+    /// 4. Serve the GATT application
     pub async fn init(&mut self) -> bluer::Result<()> {
-        self.await_pair().await?;
-        Ok(())
-    }
-
-    fn is_status_byte(byte: u8) -> bool {
-        byte & 0b1000_0000 != 0
-    }
-
-    /// TODO: THIS DOESN'T AWAIT PAIRING, REFACTOR OR RENAME
-    async fn await_pair(&mut self) -> bluer::Result<()> {
+        // Register the agent (as default and with no PIN code)
         self.agent_handle = Some(
-            self.midi_session.register_agent(Agent {
+            self.ble_session.register_agent(Agent {
                 request_default: true,
                 request_pin_code: None,
                 display_pin_code: None,
@@ -78,13 +66,15 @@ impl MidiBle {
             }).await?
         );
 
-        let adapter = self.midi_session.default_adapter().await?;
+        // Make sure the adapter is powered on and ready to go, but not discoverable or pairable outside the midi service
+        let adapter = self.ble_session.default_adapter().await?;
         adapter.set_powered(true).await?;
-        adapter.set_pairable(true).await?;
-        adapter.set_discoverable(true).await?;
+        adapter.set_pairable(false).await?;
+        adapter.set_discoverable(false).await?;
+        adapter.set_alias("AutoDrum".to_string()).await?;
 
-
-        println!("Advertising on Bluetooth adapter {} with address {}\n", adapter.name(), adapter.address().await?);
+        // Advertise the MIDI service with a low connection interval (specified <15ms by MIDI BLE spec)
+        println!("Advertising on Bluetooth adapter {} with address {}", adapter.name(), adapter.address().await?);
         let le_advertisement = Advertisement {
             advertisement_type: bluer::adv::Type::Peripheral,
             service_uuids: vec!["03B80E5A-EDE8-4B33-A751-6CE34EC4C700".parse().unwrap()].into_iter().collect(),
@@ -94,36 +84,33 @@ impl MidiBle {
             max_interval: Some(Duration::from_millis(5)),
             ..Default::default()
         };
-        println!("Advertisement: {:?}\n\n", &le_advertisement);
+        println!("Advertisement: {:#?}\n\n", &le_advertisement);
         self.advertisement_handle = Some(adapter.advertise(le_advertisement).await?);
 
-        match self.tx.send((1, 1, 1)) {
-            Ok(_) => println!("Sent over tx"),
-            Err(e) => println!("Error sending over tx: {}", e),
-        }
-
-        self.setup_midi_gatt_service().await?;
-
-        Ok(())
-    }
-
-    async fn setup_midi_gatt_service(&mut self) -> bluer::Result<()> {
-        let adapter = self.midi_session.default_adapter().await?;
+        // Serve the GATT application
         let application = self.midi_application().await;
-
         self.app_handle = Some(adapter.serve_gatt_application(application).await?);
+
         Ok(())
     }
 
+    fn is_status_byte(byte: u8) -> bool {
+        byte & 0b1000_0000 != 0
+    }
+
+    /// Create the GATT application for the MIDI service
+    ///
+    /// Characteristics:
+    /// - Read: Empty. Characteristic is required but not used. May be used in the future
+    /// - Write: MIDI data. This is the characteristic that will be used to send received MIDI data
+    ///     to the core AutoDrum application. Currently only sends note-on messages, as duration is
+    ///     handled by the AutoDrum application.
+    /// - Notify: Empty. Characteristic is required but not used.
     async fn midi_application(&self) -> Application {
         let value = Arc::new(Mutex::new(vec![0x10, 0x01, 0x01, 0x10]));
-        let value_read = value.clone();
+        // let value_read = value.clone();  // Will likely need this later if output is implemented
         let value_write = value.clone();
-        let value_notify = value.clone();
         let tx_clone = self.tx.clone();
-
-        let mut session = self.midi_session.clone();
-        let mut adapter = session.default_adapter().await.unwrap().clone();
 
         Application {
             services: vec![
@@ -138,31 +125,7 @@ impl MidiBle {
                             read: Some(CharacteristicRead {
                                 read: true,
                                 fun: Box::new(move |req| {
-                                    let value = value_read.clone();
-                                    let adapter = adapter.clone();
                                     Box::pin(async move {
-                                        let value = value.lock().await.clone();
-                                        let adapter = adapter.clone();
-
-                                        // If it's a pairing request, try to pair and trust
-                                        // TODO: Handle case where another device is already connected
-                                        if vec![16, 1, 1, 16] == value {
-                                            println!("Pairing request");
-                                            let device = adapter.device(req.device_address).unwrap();
-                                            if let Err(err) = device.pair().await {
-                                                println!("Did not pair: {:?}", err.message);
-                                            } else {
-                                                println!("Paired!");
-                                            }
-                                            if let Err(err) = device.set_trusted(true).await {
-                                                println!("Did not trust: {:?}", err.message);
-                                            } else {
-                                                println!("Trusted!");
-                                            }
-                                        } else {
-                                            println!("Not pairing request");
-                                        }
-                                        println!("Read request {:?} with value {:x?}", &req, &value);
                                         Ok([].to_vec())
                                     })
                                 }),
@@ -186,7 +149,7 @@ impl MidiBle {
                                                 // if we just finished a note-on message group, send them over tx
                                                 if midi_data.len() > 0 {
                                                     if last_status == 0x90 {
-
+                                                        // split midi data into chunks of 2 bytes (note number and velocity) and send over tx (to be handled by AutoDrum)
                                                         midi_data.chunks(2).for_each(|pair| {
                                                             let note_number = pair[0];
                                                             let velocity = pair[1];
@@ -197,7 +160,6 @@ impl MidiBle {
                                                 }
                                                 last_status = *byte;
                                             } else {
-                                                // TODO: HERE, split data into pairs and send each pair over tx
                                                 midi_data.push(*byte);
                                             }
                                         }
@@ -209,10 +171,7 @@ impl MidiBle {
                             notify: Some(CharacteristicNotify {
                                 notify: true,
                                 method: CharacteristicNotifyMethod::Fun(Box::new(move |mut notifier| {
-                                    println!("Notification session start with confirming={:?}", notifier.confirming());
-                                    Box::pin(async move {
-                                        println!("Notification guy run")
-                                    })
+                                    Box::pin(async move {})
                                 })),
                                 ..Default::default()
                             }),
